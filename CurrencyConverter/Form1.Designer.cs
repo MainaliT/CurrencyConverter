2b6cbd72989df4c0f050cf3c92b43eae15c86e86
Tek Mainali <parulmainali@gmail.com>
2016-01-07 20:09:50 -0500

    Currency Converter

---
+++ CurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Rbtn1 = new System.Windows.Forms.RadioButton();
+            this.Rbtn2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Rbtn1
+            // 
+            this.Rbtn1.AutoSize = true;
+            this.Rbtn1.Location = new System.Drawing.Point(62, 41);
+            this.Rbtn1.Name = "Rbtn1";
+            this.Rbtn1.Size = new System.Drawing.Size(103, 17);
+            this.Rbtn1.TabIndex = 0;
+            this.Rbtn1.TabStop = true;
+            this.Rbtn1.Text = "Dollors To Euros";
+            this.Rbtn1.UseVisualStyleBackColor = true;
+            this.Rbtn1.CheckedChanged += new System.EventHandler(this.Rbtn1_CheckedChanged);
+            // 
+            // Rbtn2
+            // 
+            this.Rbtn2.AutoSize = true;
+            this.Rbtn2.Location = new System.Drawing.Point(62, 75);
+            this.Rbtn2.Name = "Rbtn2";
+            this.Rbtn2.Size = new System.Drawing.Size(98, 17);
+            this.Rbtn2.TabIndex = 1;
+            this.Rbtn2.TabStop = true;
+            this.Rbtn2.Text = "Euros To Dollor";
+            this.Rbtn2.UseVisualStyleBackColor = true;
+            this.Rbtn2.CheckedChanged += new System.EventHandler(this.Rbtn2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(62, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convert Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(289, 271);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Rbtn2);
+            this.Controls.Add(this.Rbtn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton Rbtn1;
+        private System.Windows.Forms.RadioButton Rbtn2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CurrencyConverter/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        double result;
+        double input;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Rbtn1_CheckedChanged(object sender, EventArgs e)
+        {
+           // label1.Text = "Dollars";
+            //label2.Text = "Euros";
+
+        }
+
+        private void Rbtn2_CheckedChanged(object sender, EventArgs e)
+        {
+           // label1.Text = "Euros";
+           // label2.Text = "Dollars";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Rbtn1.Checked)
+            {
+                input = Double.Parse(textBox1.Text);
+                result = input * 0.88056;
+            }
+            else if (Rbtn2.Checked)
+            {
+                input = Double.Parse(textBox1.Text);
+                result = input / 0.88056;
+            }
+
+            // this will round up to two digit
+            if (Rbtn1.Checked)
+            {
+                textBox2.Text = "" + System.Math.Round(result, 2) + " Euro";
+            } else if (Rbtn2.Checked) {
+                textBox2.Text = "$ " + System.Math.Round(result, 2);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
